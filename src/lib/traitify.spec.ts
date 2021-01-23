625e6ef82278313cc53dbce6b56a31ece95805f7
Matthew Adams <matthew.adams@northscaler.com>
2021-01-23 09:21:56 -0600

    bang

---
+++ src/lib/traitify.spec.ts
@@ -0,0 +1,77 @@
+import { trait, superclass, Constructor } from './traitify';
+
+import test from 'ava';
+
+test('traits', (t) => {
+  interface ITaggable {
+    tag?: string;
+  }
+
+  const Taggable = <S>(superclass: Constructor<S>) =>
+    class extends superclass implements ITaggable {
+      protected _tag?: string;
+
+      get tag() {
+        return this._tag;
+      }
+
+      set tag(tag) {
+        this._doSetTag(this._testSetTag(tag));
+      }
+
+      constructor(...args: any[]) {
+        super(...args);
+      }
+
+      _testSetTag(tag?: string) {
+        return tag;
+      }
+
+      _doSetTag(tag?: string) {
+        this._tag = tag;
+        return this;
+      }
+    };
+
+  class Point extends trait(Taggable) {
+    constructor(public x: number, public y: number) {
+      super(...arguments);
+    }
+
+    _testSetTag(tag?: string) {
+      tag = super._testSetTag(tag);
+
+      if (!tag) throw new Error('no tag given');
+      else return tag.toLowerCase();
+    }
+  }
+
+  const point = new Point(10, 20);
+  point.tag = 'hello';
+
+  t.is(point.tag, 'hello');
+  t.throws(() => point.tag = '');
+
+  class Base {
+    something: string = 'I am a base';
+  }
+
+  class Sub extends superclass(Base).with(Taggable).done() {
+    constructor() {
+      super(...arguments);
+    }
+
+    _testSetTag(tag?: string): string | undefined {
+      tag = super._testSetTag(tag);
+
+      if (tag === 'throw') throw new Error('illegal tag value');
+      return tag;
+    }
+  }
+
+  const sub = new Sub();
+  sub.tag = 'sub';
+
+  t.is(sub.tag, 'sub');
+  t.throws(() => sub.tag = 'throw');
+});
---
+++ src/lib/traitify.ts
@@ -0,0 +1,50 @@
+/**
+ * Type definition of a constructor.
+ */
+export type Constructor<T> = new(...args: any[]) => T;
+
+/**
+ * A "trait" is a function that takes a superclass `S` and returns new class of type `S & T`.
+ */
+export type Trait<T> = <S>(superclass: Constructor<S>) => Constructor<S & T>
+
+/**
+ * Convenient function when defining a class that
+ * * extends a superclass, and
+ * * expresses one or more traits.
+ */
+export const superclass = <S>(superclass?: Constructor<S>) => new TraitBuilder(superclass)
+
+/**
+ * Convenient function to be used when defining a class that
+ * * does not extend a superclass, and
+ * * expresses one or more traits.
+ */
+export const trait = <T>(t: Trait<T>) => superclass().with(t).done()
+
+/**
+ * A convenient trait applier class.
+ */
+class TraitBuilder<S> {
+  superclass?: Constructor<S>;
+
+  constructor (superclass?: Constructor<S>) {
+    this.superclass = superclass
+  }
+
+  /**
+   * Applies the trait to the current superclass then returns a new `TraitBuilder`.
+   * @param trait The trait that the current superclass should express.
+   */
+  with <T>(trait: Trait<T>) {
+    // we have to return a new builder here because there's no way to take a collection of traits of differing types.
+    return new TraitBuilder(trait(this.superclass || class {}))
+  }
+
+  /**
+   * Return the class with all traits expressed.
+   */
+  done() {
+    return this.superclass || class {}
+  }
+}
